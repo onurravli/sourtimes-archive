f57fe009913ff32051228eddc2e04f15ca8828cf
Onur Ravli <onur@onurravli.com>
2024-02-02 20:36:04 +0300

    chore(backend): create author controller

---
+++ packages/backend/src/controllers/author.controller.ts
@@ -0,0 +1,16 @@
+class AuthorController {
+  public async get() {
+    // TODO: Implement method
+  }
+  public async post() {
+    // TODO: Implement method
+  }
+  public async put() {
+    // TODO: Implement method
+  }
+  public async delete() {
+    // TODO: Implement method
+  }
+}
+
+export { AuthorController };
