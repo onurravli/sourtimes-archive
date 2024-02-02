ef7fd3c0a96cbb69495f6aa4d9296be01d08a30c
Onur Ravli <onur@onurravli.com>
2024-02-02 23:57:28 +0300

    chore(backend): update entry model to add author & topic fields

--- packages/backend/src/models/entry.model.ts
+++ packages/backend/src/models/entry.model.ts
@@ -1,13 +1,15 @@
 class Entry {
   constructor(
     public content: string,
-    public authorId: string,
+    public author: string,
+    public topic: string,
     public createdAt: Date,
     public updatedAt: Date,
     public favorites: string[],
   ) {
     this.content = content;
-    this.authorId = authorId;
+    this.author = author;
+    this.topic = topic;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
     this.favorites = favorites;
