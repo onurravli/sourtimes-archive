5a538bd0dbbb7f0f1858e6850f3d70478f71c480
Onur Ravli <onur@onurravli.com>
2024-02-02 16:47:05 +0300

    chore(backend): create entry router

---
+++ packages/backend/src/routers/entry.router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { EntryController } from '../controllers';
+
+const entryRouter: Router = Router();
+const entryController: EntryController = new EntryController();
+
+entryRouter.get('/:id', entryController.get);
+entryRouter.post('/:id', entryController.post);
+entryRouter.put('/:id', entryController.put);
+entryRouter.delete('/:id', entryController.delete);
+
+export { entryRouter };
