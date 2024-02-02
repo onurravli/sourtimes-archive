95296b54607ccb9fc7bc68adb592b8bcad9d7339
Onur Ravli <onur@onurravli.com>
2024-02-03 00:47:37 +0300

    docs: add comments to methods

--- packages/backend/src/controllers/entry.controller.ts
+++ packages/backend/src/controllers/entry.controller.ts
@@ -1,9 +1,29 @@
 import type { Request, Response } from 'express';
 
 class EntryController {
+  /**
+   * Get an entry by its ID.
+   * @param req Request
+   * @param res Response
+   */
   public async get(req: Request, res: Response) {}
+  /**
+   * Create a new entry.
+   * @param req Request
+   * @param res Response
+   */
   public async post(req: Request, res: Response) {}
+  /**
+   * Update an existing entry.
+   * @param req Request
+   * @param res Response
+   */
   public async put(req: Request, res: Response) {}
+  /**
+   * Delete an entry by its ID.
+   * @param req Request
+   * @param res Response
+   */
   public async delete(req: Request, res: Response) {}
 }
 
