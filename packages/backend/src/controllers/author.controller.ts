4c340cf508b28dac946cef4ed3dbbecf1dbe6c53
Onur Ravli <onur@onurravli.com>
2024-02-03 01:25:36 +0300

    docs: add comments to methods

--- packages/backend/src/controllers/author.controller.ts
+++ packages/backend/src/controllers/author.controller.ts
@@ -1,16 +1,30 @@
+import { Request, Response } from 'express';
+
 class AuthorController {
-  public async get() {
-    // TODO: Implement method
-  }
-  public async post() {
-    // TODO: Implement method
-  }
-  public async put() {
-    // TODO: Implement method
-  }
-  public async delete() {
-    // TODO: Implement method
-  }
+  /**
+   * Get an author by its ID.
+   * @param req Request
+   * @param res Response
+   */
+  public async get(req: Request, res: Response) {}
+  /**
+   * Create a new author.
+   * @param req Request
+   * @param res Response
+   */
+  public async post(req: Request, res: Response) {}
+  /**
+   * Update an existing author.
+   * @param req Request
+   * @param res Response
+   */
+  public async put(req: Request, res: Response) {}
+  /**
+   * Delete an author by its ID.
+   * @param req Request
+   * @param res Response
+   */
+  public async delete(req: Request, res: Response) {}
 }
 
 export { AuthorController };
--- packages/backend/src/controllers/topic.controller.ts
+++ packages/backend/src/controllers/topic.controller.ts
@@ -1,16 +1,30 @@
+import { Request, Response } from 'express';
+
 class TopicController {
-  public async get() {
-    // TODO: Implement method
-  }
-  public async post() {
-    // TODO: Implement method
-  }
-  public async put() {
-    // TODO: Implement method
-  }
-  public async delete() {
-    // TODO: Implement method
-  }
+  /**
+   * Get a topic by its ID.
+   * @param req Request
+   * @param res Response
+   */
+  public async get(req: Request, res: Response) {}
+  /**
+   * Create a new topic.
+   * @param req Request
+   * @param res Response
+   */
+  public async post(req: Request, res: Response) {}
+  /**
+   * Update an existing topic.
+   * @param req Request
+   * @param res Response
+   */
+  public async put(req: Request, res: Response) {}
+  /**
+   * Delete a topic by its ID.
+   * @param req Request
+   * @param res Response
+   */
+  public async delete(req: Request, res: Response) {}
 }
 
 export { TopicController };
