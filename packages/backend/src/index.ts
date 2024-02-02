0ec9e19bcbf9eab1b2e8dcbf2b98399f66092f90
Onur Ravli <onur@onurravli.com>
2024-02-03 00:35:28 +0300

    chore(backend): add mongodb methods to main function

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,23 @@
 import express, { Application, Request, Response } from 'express';
 import { env } from './utils';
 import { entryRouter } from './routers';
-import { guvercin } from './services';
+import { guvercin, mongodb } from './services';
 
 const app: Application = express();
 const port: number = parseInt(env.PORT as string) || 3000;
 
 const main = async () => {
+  try {
+    await mongodb.connectToDb();
+    guvercin.success('Connected to MongoDB.');
+  } catch (error) {
+    guvercin.error(`Couldn't connect to MongoDB. (${(error as Error).message})`);
+    app.all('*', (req: Request, res: Response) => {
+      return res.status(500).json({
+        error: `Couldn't connect to MongoDB. (${(error as Error).message})`,
+      });
+    });
+  }
   try {
     app.use(express.json());
     app.use('/entry', entryRouter);
