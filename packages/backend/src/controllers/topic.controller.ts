46dc132c3f2077d1503f154c99fdaab9f9623598
Onur Ravli <onur@onurravli.com>
2024-02-02 20:36:15 +0300

    chore(backend): create topic controller

---
+++ packages/backend/src/controllers/topic.controller.ts
@@ -0,0 +1,16 @@
+class TopicController {
+  public async get() {
+    // TODO: Implement method
+  }
+  public async post() {
+    // TODO: Implement method
+  }
+  public async put() {
+    // TODO: Implement method
+  }
+  public async delete() {
+    // TODO: Implement method
+  }
+}
+
+export { TopicController };
