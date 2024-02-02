72c0ce30a1b58d499822db04ee6a9d171f1e2016
Onur Ravli <onur@onurravli.com>
2024-02-03 01:17:05 +0300

    chore(backend): add mongoClient.connect() to connectToDb function

--- packages/backend/src/services/mongodb.service.ts
+++ packages/backend/src/services/mongodb.service.ts
@@ -9,6 +9,7 @@ const collections: {
 
 const connectToDb = async () => {
   const mongoClient: MongoClient = new MongoClient(env.MONGODB_CONNECTION_STRING as string);
+  await mongoClient.connect();
   const db: Db = mongoClient.db(env.MONGODB_DATABASE_NAME as string);
   const authorsCollection: Collection = db.collection(env.MONGODB_AUTHORS_COLLECTION as string);
   const entriesCollection: Collection = db.collection(env.MONGODB_ENTRIES_COLLECTION as string);
