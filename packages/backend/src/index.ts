834d007cd8d91bc9fc4ad534de1ffbc12147634e
Onur Ravli <onur@onurravli.com>
2024-02-03 00:33:52 +0300

    chore(backend): remove postgres functions from main function

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,56 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import { env } from './utils';
 import { entryRouter } from './routers';
-import { guvercin, postgres } from './services';
+import { guvercin } from './services';
 
 const app: Application = express();
 const port: number = parseInt(env.PORT as string) || 3000;
 
 const main = async () => {
-  try {
-    await postgres.pool.connect();
-    guvercin.success('Connected to the database.');
-  } catch (error) {
-    guvercin.error(`Couldn't connect to the database. (${(error as Error).message})`);
-    app.all('*', (req: Request, res: Response) => {
-      return res.status(500).json({
-        error: `Couldn't connect to the database. (${(error as Error).message})`,
-      });
-    });
-  }
-  try {
-    await postgres.createEntriesTable();
-    guvercin.success('Created entries table.');
-  } catch (error) {
-    guvercin.error(`Couldn't create entries table. (${(error as Error).message})`);
-    app.all('*', (req: Request, res: Response) => {
-      return res.status(500).json({
-        error: `Couldn't create entries table. (${(error as Error).message})`,
-      });
-    });
-  }
-  try {
-    await postgres.createAuthorsTable();
-    guvercin.success('Created authors table.');
-  } catch (error) {
-    guvercin.error(`Couldn't create authors table. (${(error as Error).message})`);
-    app.all('*', (req: Request, res: Response) => {
-      return res.status(500).json({
-        error: `Couldn't create authors table. (${(error as Error).message})`,
-      });
-    });
-  }
-  try {
-    await postgres.createTopicsTable();
-    guvercin.success('Created topics table.');
-  } catch (error) {
-    guvercin.error(`Couldn't create topics table. (${(error as Error).message})`);
-    app.all('*', (req: Request, res: Response) => {
-      return res.status(500).json({
-        error: `Couldn't create topics table. (${(error as Error).message})`,
-      });
-    });
-  }
   try {
     app.use(express.json());
     app.use('/entry', entryRouter);
