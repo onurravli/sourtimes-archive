0e27fade8b7f52425db6499eb7ad24942f6d1b76
Onur Ravli <onur@onurravli.com>
2024-02-02 20:51:47 +0300

    feat(backend): update generateId util to generate uuid instead of hex

--- packages/backend/src/utils/generateId.util.ts
+++ packages/backend/src/utils/generateId.util.ts
@@ -1,7 +1,7 @@
-import * as crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
 
-const generateId = (length?: number) => {
-  const id = crypto.randomBytes(length || 25).toString('hex');
+const generateId = () => {
+  const id = uuidv4();
   return id;
 };
 
