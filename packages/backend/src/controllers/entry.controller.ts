40a3cb5372e364b386dc8dddd0a304c4f5bd544c
Onur Ravli <onur@onurravli.com>
2024-02-03 01:25:12 +0300

    chore(backend): add GET /entry/:id method

--- packages/backend/src/controllers/entry.controller.ts
+++ packages/backend/src/controllers/entry.controller.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { mongodb } from '../services';
+import { ObjectId } from 'mongodb';
 
 class EntryController {
   /**
@@ -6,7 +8,25 @@ class EntryController {
    * @param req Request
    * @param res Response
    */
-  public async get(req: Request, res: Response) {}
+  public async get(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const entry = await mongodb.collections.entries?.findOne({
+        _id: new ObjectId(id),
+      });
+      if (entry) {
+        return res.status(200).json(entry);
+      }
+      return res.status(404).json({
+        error: 'Entry not found.',
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: 'Internal server error.',
+        detail: error,
+      });
+    }
+  }
   /**
    * Create a new entry.
    * @param req Request
