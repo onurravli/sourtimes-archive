de835934a16cfb5a4c56c6ca88dc3c6287576e4f
Onur Ravli <onur@onurravli.com>
2024-02-02 16:33:19 +0300

    chore(backend): create entry controller

---
+++ packages/backend/src/controllers/entry.controller.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from 'express';
+
+class EntryController {
+  public async get(req: Request, res: Response) {}
+  public async post(req: Request, res: Response) {}
+  public async put(req: Request, res: Response) {}
+  public async delete(req: Request, res: Response) {}
+}
+
+export { EntryController };
