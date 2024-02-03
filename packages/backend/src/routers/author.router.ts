8622c239920d22bb3a733a10bc2961dca85a0e59
Onur Ravli <onur@onurravli.com>
2024-02-03 12:50:32 +0300

    chore(backend): update author route to change id to nick

--- packages/backend/src/routers/author.router.ts
+++ packages/backend/src/routers/author.router.ts
@@ -5,9 +5,9 @@ import { joi } from '../middlewares';
 const authorRouter: Router = Router();
 const authorController: AuthorController = new AuthorController();
 
-authorRouter.get('/:id', joi.author.get, authorController.get);
+authorRouter.get('/:nick', joi.author.get, authorController.get);
 authorRouter.post('/', joi.author.post, authorController.post);
-authorRouter.put('/:id', joi.author.put, authorController.put);
-authorRouter.delete('/:id', joi.author.delete, authorController.delete);
+authorRouter.put('/:nick', joi.author.put, authorController.put);
+authorRouter.delete('/:nick', joi.author.delete, authorController.delete);
 
 export { authorRouter };
